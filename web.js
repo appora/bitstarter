0bf70aabaf0f8db4cede345ce4c80bc87fe5ce7c
Eloi <eloi@Baby.(none)>
2013-07-23 07:18:38 +0100

    Modified web.js to read from  index.html

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 var fs = require('fs');
-var html = new Buffer(fs.readFile('index.html'), "utf-8")
+//var html = new Buffer(fs.readFile('index.html'), "utf-8")
 app.get('/', function(request, response) {
     response.send("Test");
 });
