ceaa8f7ae4f7e09c42c290b5c577b359b8f70b8f
Eloi <eloi@Baby.(none)>
2013-07-23 07:02:33 +0100

    Added index.html

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
 app.use(express.logger());
-
+var fs = require('fs');
+var html = new Buffer(fs.readFile('index.html'), "utf-8")
 app.get('/', function(request, response) {
-  response.send('Hello World 2 ! ');
+    response.send(html.tostring());
 });
 
 var port = process.env.PORT || 5000;
